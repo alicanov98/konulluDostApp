a74359b4fbebcceb5155369ad12105321cab5e18
alicanov98 <alicanov1998@gmail.com>
2024-03-12 06:34:46 +0400

    Changes files

--- src/hooks/useTabBarVisibility.tsx
+++ src/hooks/useTabBarVisibility.tsx
@@ -18,6 +18,11 @@ const tabHiddenRoutes = [
   'OtpScreen',
   'NewPasswordScreen',
   'ClubDetailsScreen',
+  'AboutAppScreen',
+  'AboutVolunteerScreen',
+  'DeviceScreen',
+  'RefreshScreen',
+  'ProfileScreen',
 ];
 
 const useTabBarVisibility = () => {
