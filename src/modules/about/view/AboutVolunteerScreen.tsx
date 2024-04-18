2eda67971e90efbafd4f085855df7d78269467c8
alicanov98 <alicanov1998@gmail.com>
2024-04-18 15:09:55 +0400

    some changed

--- src/modules/about/view/AboutVolunteerScreen.tsx
+++ src/modules/about/view/AboutVolunteerScreen.tsx
@@ -1,8 +1,15 @@
-import { SafeAreaView, StatusBar, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 import React from 'react';
 import GlobalStyles from '../../../assets/globalStyles/styles.ts';
-import CarusellTwo from '../../../components/swiper/CarusellTwo.tsx';
+import {Controller} from 'react-hook-form';
+import {SelectList} from 'react-native-dropdown-select-list';
 
 const AboutVolunteerScreen = () => {
   return (
@@ -29,7 +36,57 @@ const AboutVolunteerScreen = () => {
           Könüllü Dost Haqqında!
         </Text>
       </View>
-      <View style={{width: '100%', height: 300, backgroundColor: 'red'}}>
+      <View style={{top: 20, alignItems: 'center', flex: 1}}>
+        <TouchableOpacity
+          style={{
+            width: 400,
+            height: 50,
+            borderRadius: 10,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            borderColor: '#000',
+            borderWidth: 1,
+          }}>
+          <View
+            style={{
+              backgroundColor: '#fff',
+              width: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: 30,
+                height: 30,
+                borderColor: '#000',
+                borderWidth: 1,
+                borderRadius: 999,
+                fontSize: 20,
+              }}>
+              -
+            </Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: '#494747',
+              width: 350,
+              height: 50,
+              paddingHorizontal: 10,
+              borderTopRightRadius: 10,
+              borderBottomRightRadius: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{color: '#fff', fontSize: 17}}>
+              Tibbi-Sosial Ekspert Komissiyası
+            </Text>
+            <Text style={{color: '#fff', fontSize: 17}}>T</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
